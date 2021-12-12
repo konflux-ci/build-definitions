b63dac346bdb1cd07aaf2f19220e8d99933d7b22
John Duimovich <jduimovich@gmail.com>
2021-12-12 09:23:29 -0500

    remove redhat-appstudio from devmode
    - safer option

--- tasks/appstudio-utils/dev-build.sh
+++ tasks/appstudio-utils/dev-build.sh
@@ -1,18 +1,13 @@
-# local build script
+# local dev build script
 
-# to prevent forks from accidentally pushing to redhat-appstudio
-# do no allow even authorized users to accidentally use this script to do so. 
-REPO_USER=$(git config --get remote.origin.url | cut -d '/' -f 4) 
-if [ "$REPO_USER" = "redhat-appstudio" ]; then
-    echo "Using redhat-appstudio quay.io user to push results "
-    docker build -t quay.io/redhat-appstudio/appstudio-utils:v0.1 .
-    docker push quay.io/redhat-appstudio/appstudio-utils:v0.1 
-    exit 0 
-fi 
 if [  -z "$MY_QUAY_USER" ]; then
     echo "MY_QUAY_USER is not set, skip this build."
     exit 0
-fi 
+fi
+if [ "$MY_QUAY_USER" = "redhat-appstudio" ]; then
+    echo "Cannot use devmode as redhat-appstudio user "
+    exit 1  
+fi
 BUILD_TAG=$(date +"%Y-%m-%d-%H%M%S") 
 IMG="quay.io/$MY_QUAY_USER/appstudio-utils:$BUILD_TAG"
 echo "Using $MY_QUAY_USER to push results "
