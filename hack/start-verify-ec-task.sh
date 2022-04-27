64303dfd2f2c3fc3b6553358dff06c2c78106114
Luiz Carvalho <lucarval@redhat.com>
2022-04-27 12:25:03 -0400

    Add pipeline run param to start-verify-ec-task
    Signed-off-by: Luiz Carvalho <lucarval@redhat.com>

--- hack/start-verify-ec-task.sh
+++ hack/start-verify-ec-task.sh
@@ -33,6 +33,8 @@ set -euo pipefail
 
 IMAGE_REF="$1"
 
+PIPELINERUN_NAME="$2"
+
 PUBLIC_KEY_SECRET="${PUBLIC_KEY_SECRET:-cosign-public-key}"
 
 NAMESPACE="$(oc get sa default -o jsonpath='{.metadata.namespace}')"
@@ -76,6 +78,9 @@ spec:
 
     - name: PUBLIC_KEY
       value: k8s://$NAMESPACE/$PUBLIC_KEY_SECRET
+
+    - name: PIPELINERUN_NAME
+      value: $PIPELINERUN_NAME
 
     # Modify these defaults as required
     #- name: POLICY_REPO
