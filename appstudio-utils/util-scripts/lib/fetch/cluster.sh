8e937d60c6fef66175e4dc08a73a03d68b79e83b
Zoran Regvart <zoran@regvart.com>
2022-05-12 13:37:35 +0200

    Use EnterpriseContractPolicy in v-e-c
    This adds `POLICY_CONFIGURATION` parameter to the
    `verify-enterprise-contract` Tekton task used to point to the
    EnterpriseContractPolicy custom resource with which the policy
    configuration for executing the task is governed. Namely, how to fetch
    the policy files and configuration of non-blocking policy rules.
    
    With respect to current demo scripts, backward compatibility is
    maintained. So configuring non-blocking policy rules via ConfigMap and
    fetching from Git using `POLICY_REPO` and `POLICY_REPO_REF` is still
    possible. But note that `POLICY_CONFIGURATION` takes precedence over
    those.
    
    Ref. https://issues.redhat.com/browse/HACBS-244

--- appstudio-utils/util-scripts/lib/fetch/cluster.sh
+++ appstudio-utils/util-scripts/lib/fetch/cluster.sh
@@ -21,9 +21,46 @@ _default-policy-config() {
   echo '{"non_blocking_checks":["not_useful"]}'
 }
 
-# Placeholder. In future it will likely come from
-# an EnterpriseContractPolicy crd, see HACBS-244
+# Splits the given string on '/' and returns the second part
+cr-name() {
+  echo "${1//*\//}"
+}
+
+# Checks if the given string has a '/' and if it does splits
+# it on '/' and returns the "-n <first part>"
+cr-namespace-argument() {
+  if [[ "$1" != */* ]]; then
+    return
+  fi
+
+  local namespace="${1//\/*/}"
+  if [[ -n "${namespace}" ]]; then
+    echo "-n ${namespace}"
+  fi
+}
+
+# If given $1, looks up the ECP custom resource
 save-policy-config() {
+  local namespace_arg
+  namespace_arg=$(cr-namespace-argument "${1:-}")
+  local args
+  args=(${namespace_arg} "$(cr-name "${1:-}")") # intentionally not quoting the $namespace_arg so it expands
+  if ! non_blocking_data=$(kubectl get enterprisecontractpolicies.appstudio.redhat.com "${args[*]}" -o jsonpath='{.spec.exceptions.nonBlocking}'); then
+    local namespace=${namespace_arg#-n }
+    echo "ERROR: unable to find the ec-policy EnterpriseContractPolicy in namespace ${namespace:-$(kubectl config view --minify -o jsonpath='{..namespace}')}" 1>&2
+    # TODO remove this condition once $1 is mandatory, i.e. we no longer
+    # use the config map or the fallback policy below
+    if [[ $# -gt 0 ]]; then # remove to fail unconditionally
+      return 1
+    fi
+  else
+    local non_blocking_data_file
+    non_blocking_data_file="$( json-data-file config policy non_blocking_checks)"
+    echo "$non_blocking_data" | jq > "${non_blocking_data_file}"
+    return 0
+  fi
+
+  # TODO remove the below lines once the demos don't depend on this
   # Note: the namespace the task is running in needs to have the ec-policy ConfigMap
   { _policy-config-from-configmap || _default-policy-config ; } | jq > "$( json-data-file config policy )"
 }
--- appstudio-utils/util-scripts/lib/fetch/git.sh
+++ appstudio-utils/util-scripts/lib/fetch/git.sh
@@ -16,6 +16,26 @@ git-fetch-repo() {
 }
 
 git-fetch-policies() {
+  local args
+  args=($(cr-namespace-argument "${1:-}") $(cr-name "${1:-}"))
+
+  local repository_and_ref
+  if repository_and_ref=$(kubectl get enterprisecontractpolicies.appstudio.redhat.com "${args[*]}" -o jsonpath='{.spec.sources[0].git.repository}#{.spec.sources[0].git.revision}'); then
+    # TODO this overrides POLICY_REPO and POLICY_REPO_REF that might be given
+    # via Task parameters, when we no longer rely on these parameters, i.e.
+    # we exclusively use the ECP custom resource refactor this to use local
+    # variables instead
+    POLICY_REPO="${repository_and_ref//#*/}"
+    POLICY_REPO_REF="${repository_and_ref//*#/}"
+  else
+    # when POLICY_REPO and POLICY_REPO_REF parameters are no longer defined
+    # for the Task, i.e. we exclusively use the ECP custom resource we
+    # should fail here, uncomment this line and remove the uncommented
+    # lines below it
+    # return 1 # uncomment to fail
+    :          # remove noop
+  fi
+
   echo "Fetching policies from $POLICY_REPO_REF at $POLICY_REPO"
   echo "sha: $( git-fetch-repo $POLICY_REPO $POLICY_REPO_REF $POLICIES_DIR )"
 
