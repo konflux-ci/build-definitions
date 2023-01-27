cf531dafc961bba8390c6c3a0dc817be87b8bb6a
Zoran Regvart <zoran@regvart.com>
2023-01-27 13:20:10 +0100

    check-task-pipeline-bundle-repos.sh check for PRs
    Passes the pull request commit id to the
    `check-task-pipeline-repo-existence` step to make the validation
    consider pull request image references; otherwise the check checks for
    images that are have resulted from changes pushed to the default branch.
    
    The URL being checked for existence now checks against the Docker
    registry API, the quay.io REST API has no endpoint to check for
    existence of tags. Also the non-pull request artifacts are checked for
    their correct tag/version now.

--- .tekton/scripts/check-task-pipeline-bundle-repos.sh
+++ .tekton/scripts/check-task-pipeline-bundle-repos.sh
@@ -1,4 +1,8 @@
-#!/usr/bin/bash -e
+#!/usr/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$SCRIPTDIR/../.."
@@ -6,8 +10,15 @@ cd "$SCRIPTDIR/../.."
 QUAY_ORG=redhat-appstudio-tekton-catalog
 
 locate_bundle_repo() {
-    local -r repo_name=$1
-    curl -I -s -L -w "%{http_code}\n" -o /dev/null "https://quay.io/api/v1/repository/${QUAY_ORG}/${repo_name}"
+    local -r type="$1"
+    local -r object="$2"
+    local -r version="${3}"
+
+    if [ "${PULL_REQUEST:-0}" -ne 1 ]; then
+        curl -I -s -L -w "%{http_code}\n" -o /dev/null "https://quay.io/v2/${QUAY_ORG}/${type}-${object}/manifests/${version}"
+    else
+        curl -I -s -L -w "%{http_code}\n" -o /dev/null "https://quay.io/v2/${QUAY_ORG}/pull-request-builds/manifests/${object}-${version}"
+    fi
 }
 
 has_missing_repo=
@@ -17,7 +28,9 @@ echo "Checking existence of task and pipeline bundle repositories ..."
 # tasks
 for task_file in task/*/*/*.yaml; do
     task_name=$(oc apply --dry-run=client -f "$task_file" -o jsonpath='{.metadata.name}')
-    found=$(locate_bundle_repo "task-$task_name")
+    dir=${task_file%/*}
+    version="${dir##*/}-$(git log -n 1 --pretty=format:%H -- "${task_file}")"
+    found=$(locate_bundle_repo task "$task_name" "${version}")
     if [ "$found" != "200" ]; then
         echo "Missing task bundle repo: task-$task_name"
         has_missing_repo=yes
@@ -25,10 +38,11 @@ for task_file in task/*/*/*.yaml; do
 done
 
 # pipelines
+version="$(git rev-parse HEAD)"
 for pl_name in $(oc kustomize pipelines/ | oc apply --dry-run=client -f - -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}'); do
-    found=$(locate_bundle_repo "pipeline-$pl_name")
+    found=$(locate_bundle_repo pipeline "$pl_name" "${version}")
     if [ "$found" != "200" ]; then
-        echo "Missing pipeline bundle repo: pipeline-$pl_name"
+        echo "Missing pipeline bundle repo: pipeline-$pl_name ${version}"
         has_missing_repo=yes
     fi
 done
