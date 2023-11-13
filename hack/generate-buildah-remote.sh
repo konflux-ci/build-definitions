d56dae1508dca02e614a27a29b4d3ee54d223874
Chenxiong Qi <cqi@redhat.com>
2023-11-13 20:53:39 +0800

    Update multi-arch-controller image
    Signed-off-by: Chenxiong Qi <cqi@redhat.com>

--- hack/generate-buildah-remote.sh
+++ hack/generate-buildah-remote.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-podman run -v "$SCRIPTDIR"/..:/data quay.io/redhat-user-workloads/rhtap-build-tenant/multi-arch-controller/multi-arch-controller:taskgen-d1a5fd1572512ee26d0546b287a491f24a84aba9  --buildah-task=/data/task/buildah/0.1/buildah.yaml --remote-task=/data/task/buildah-remote/0.1/buildah-remote.yaml
+IMG=quay.io/redhat-user-workloads/rhtap-build-tenant/multi-arch-controller/multi-arch-controller:taskgen-57750ec21414607fa20acdef7984f32bbb7730af
+
+podman run -v "$SCRIPTDIR"/..:/data $IMG \
+       --buildah-task=/data/task/buildah/0.1/buildah.yaml \
+       --remote-task=/data/task/buildah-remote/0.1/buildah-remote.yaml
