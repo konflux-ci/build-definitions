293ad90bb1f824a91d287cd63456af0db1a78689
Chenxiong Qi <cqi@redhat.com>
2023-11-14 14:43:41 +0800

    Add :Z suffix to volume mount
    Signed-off-by: Chenxiong Qi <cqi@redhat.com>

--- hack/generate-buildah-remote.sh
+++ hack/generate-buildah-remote.sh
@@ -3,6 +3,6 @@
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 IMG=quay.io/redhat-user-workloads/rhtap-build-tenant/multi-arch-controller/multi-arch-controller:taskgen-57750ec21414607fa20acdef7984f32bbb7730af
 
-podman run -v "$SCRIPTDIR"/..:/data $IMG \
+podman run -v "$SCRIPTDIR"/..:/data:Z $IMG \
        --buildah-task=/data/task/buildah/0.1/buildah.yaml \
        --remote-task=/data/task/buildah-remote/0.1/buildah-remote.yaml
