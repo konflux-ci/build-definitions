ea1aa09dc49c3fec5382924560ce4480cecc4d32
Adam Cmiel <acmiel@redhat.com>
2024-07-15 08:45:55 +0200

    Add buildah-remote[-oci-ta] 0.2
    Steps:
    * Add version 0.2 to hack/generate-buildah-remote.sh
    * Run hack/generate-buildah-remote.sh
    * Copy MIGRATION.md from buildah[-oci-ta]/0.2
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- hack/generate-buildah-remote.sh
+++ hack/generate-buildah-remote.sh
@@ -5,8 +5,9 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $SCRIPTDIR/../task-generator
 go build -o /tmp/remote-generator ./remote/main.go
 
-
-/tmp/remote-generator --buildah-task=$SCRIPTDIR/../task/buildah/0.1/buildah.yaml \
-       --remote-task=$SCRIPTDIR/../task/buildah-remote/0.1/buildah-remote.yaml
-/tmp/remote-generator --buildah-task=$SCRIPTDIR/../task/buildah-oci-ta/0.1/buildah-oci-ta.yaml \
-       --remote-task=$SCRIPTDIR/../task/buildah-remote-oci-ta/0.1/buildah-remote-oci-ta.yaml
+for version in 0.1 0.2; do
+    /tmp/remote-generator --buildah-task=$SCRIPTDIR/../task/buildah/"$version"/buildah.yaml \
+        --remote-task=$SCRIPTDIR/../task/buildah-remote/"$version"/buildah-remote.yaml
+    /tmp/remote-generator --buildah-task=$SCRIPTDIR/../task/buildah-oci-ta/"$version"/buildah-oci-ta.yaml \
+        --remote-task=$SCRIPTDIR/../task/buildah-remote-oci-ta/"$version"/buildah-remote-oci-ta.yaml
+done
