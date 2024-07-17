f106a86817a11192393e35df4424274fbc61c5fb
Adam Cmiel <acmiel@redhat.com>
2024-07-17 14:54:45 +0200

    generate-buildah-remote: fix shellcheck warnings
    And use curly-brackets style for variables
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- hack/generate-buildah-remote.sh
+++ hack/generate-buildah-remote.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+set -euo pipefail
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-cd $SCRIPTDIR/../task-generator
+cd "${SCRIPTDIR}/../task-generator"
 go build -o /tmp/remote-generator ./remote/main.go
 
 for version in 0.1 0.2; do
-    /tmp/remote-generator --buildah-task=$SCRIPTDIR/../task/buildah/"$version"/buildah.yaml \
-        --remote-task=$SCRIPTDIR/../task/buildah-remote/"$version"/buildah-remote.yaml
-    /tmp/remote-generator --buildah-task=$SCRIPTDIR/../task/buildah-oci-ta/"$version"/buildah-oci-ta.yaml \
-        --remote-task=$SCRIPTDIR/../task/buildah-remote-oci-ta/"$version"/buildah-remote-oci-ta.yaml
+    /tmp/remote-generator --buildah-task="${SCRIPTDIR}/../task/buildah/${version}/buildah.yaml" \
+        --remote-task="${SCRIPTDIR}/../task/buildah-remote/${version}/buildah-remote.yaml"
+    /tmp/remote-generator --buildah-task="${SCRIPTDIR}/../task/buildah-oci-ta/${version}/buildah-oci-ta.yaml" \
+        --remote-task="${SCRIPTDIR}/../task/buildah-remote-oci-ta/${version}/buildah-remote-oci-ta.yaml"
 done
