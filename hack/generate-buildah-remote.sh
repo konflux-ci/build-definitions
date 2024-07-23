420bc92bb798935d84912c88eca05af798baa8ca
arewm <arewm@users.noreply.github.com>
2024-07-23 14:51:37 -0400

    Add an architecture suffix to images pushed for multi-platform
    In order to reduce the likelihood of users accidentally forgetting to
    specify unique tags for each architecture, we can add a suffix to the
    pushed image if an arch-specific one doesn't exist.
    
    Signed-off-by: arewm <arewm@users.noreply.github.com>

--- hack/generate-buildah-remote.sh
+++ hack/generate-buildah-remote.sh
@@ -8,7 +8,7 @@ go build -o /tmp/remote-generator ./remote/main.go
 
 for version in 0.1 0.2; do
     /tmp/remote-generator --buildah-task="${SCRIPTDIR}/../task/buildah/${version}/buildah.yaml" \
-        --remote-task="${SCRIPTDIR}/../task/buildah-remote/${version}/buildah-remote.yaml"
+        --remote-task="${SCRIPTDIR}/../task/buildah-remote/${version}/buildah-remote.yaml" --task-version="$version"
     /tmp/remote-generator --buildah-task="${SCRIPTDIR}/../task/buildah-oci-ta/${version}/buildah-oci-ta.yaml" \
-        --remote-task="${SCRIPTDIR}/../task/buildah-remote-oci-ta/${version}/buildah-remote-oci-ta.yaml"
+        --remote-task="${SCRIPTDIR}/../task/buildah-remote-oci-ta/${version}/buildah-remote-oci-ta.yaml" --task-version="$version"
 done
