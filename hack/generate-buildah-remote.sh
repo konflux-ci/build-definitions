de64e9cba785a758613fdf0231e0126257ad9242
Tomas Nevrlka <tnevrlka@redhat.com>
2024-09-17 11:21:36 +0200

    move ta-generator to task-generator directory
    - ta-generator and task-generator are currently
    on the same directory level
    - move ta-generator so it's a subdirectory of the task-generator
    leading to a better file organization in the repository
    - update .gitignore so it ignores vendor directories and diff
    files used in trusted-artifacts task generation testing

--- hack/generate-buildah-remote.sh
+++ hack/generate-buildah-remote.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-cd "${SCRIPTDIR}/../task-generator"
-go build -o /tmp/remote-generator ./remote/main.go
+cd "${SCRIPTDIR}/../task-generator/remote"
+go build -o /tmp/remote-generator main.go
 
 for version in 0.1 0.2; do
     /tmp/remote-generator --buildah-task="${SCRIPTDIR}/../task/buildah/${version}/buildah.yaml" \
