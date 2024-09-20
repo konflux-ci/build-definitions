6266acc8b40584dd898dfe59c747076ac6e3f59a
Adam Cmiel <acmiel@redhat.com>
2024-09-20 13:33:22 +0200

    Don't check redhat-appstudio repo existence
    We no longer push new tasks to redhat-appstudio-tekton-catalog, we only
    push them to konflux-ci/tekton-catalog.
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- .tekton/scripts/check-task-pipeline-bundle-repos.sh
+++ .tekton/scripts/check-task-pipeline-bundle-repos.sh
@@ -8,7 +8,6 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$SCRIPTDIR/../.."
 
 CATALOG_NAMESPACES=(
-    redhat-appstudio-tekton-catalog
     konflux-ci/tekton-catalog
 )
 
