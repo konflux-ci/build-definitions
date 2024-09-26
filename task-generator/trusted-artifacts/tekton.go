ae68d586f081f7d9631330d1ec5d5d550e7c85c1
Zoran Regvart <zoran@regvart.com>
2024-09-26 13:09:24 +0200

    Kustomize support trusted-artifacts Task generator
    Adds the ability to have a `kustomization.yaml` file as the base file
    for generating the OCI-TA Task variant; if it is used as a base the
    equivalent of `kustomize build` will be run expecting the resulting
    rendered template to contain the single Task definition.

--- task-generator/trusted-artifacts/tekton.go
+++ task-generator/trusted-artifacts/tekton.go
@@ -4,17 +4,51 @@ import (
 	"bytes"
 	"io"
 	"os"
+	"path/filepath"
 	"regexp"
 
 	pipeline "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
 	"github.com/tektoncd/pipeline/pkg/substitution"
 	"github.com/zregvart/tkn-fmt/format"
+	"sigs.k8s.io/kustomize/api/krusty"
+	"sigs.k8s.io/kustomize/api/types"
+	"sigs.k8s.io/kustomize/kyaml/filesys"
 	"sigs.k8s.io/yaml"
 )
 
+func loadTask(path string) (*pipeline.Task, error) {
+	if filepath.Base(path) == "kustomization.yaml" {
+		return renderTask(filepath.Dir(path))
+	}
+
+	return readTask(path)
+}
+
 func readTask(path string) (*pipeline.Task, error) {
-	b := expectValue(os.ReadFile(path)) // #nosec G304 -- we want to read the file as Task, nothing to worry about here
+	b, err := os.ReadFile(path) // #nosec G304 -- we want to read the file as Task, nothing to worry about here
+	if err != nil {
+		return nil, err
+	}
 	b = bytes.TrimLeft(b, "---\n")
+	task := pipeline.Task{}
+	return &task, yaml.Unmarshal(b, &task)
+}
+
+func renderTask(path string) (*pipeline.Task, error) {
+	opts := krusty.MakeDefaultOptions()
+	opts.LoadRestrictions = types.LoadRestrictionsNone
+
+	kustomize := krusty.MakeKustomizer(opts)
+	result, err := kustomize.Run(filesys.MakeFsOnDisk(), path)
+	if err != nil {
+		return nil, err
+	}
+
+	b, err := result.AsYaml()
+	if err != nil {
+		return nil, err
+	}
+
 	task := pipeline.Task{}
 	return &task, yaml.Unmarshal(b, &task)
 }
