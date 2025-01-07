c1725c19a77e195cb1c9fe1d9f0374280449780f
Sushanta Das <sushanta.das.ju@gmail.com>
2025-01-07 13:27:59 +0530

    Validate kustomize build in CI

---
+++ hack/verify-manifests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+main() {
+
+    "${SCRIPT_DIR}"/build-manifests.sh
+    if [[ $(git status --porcelain) ]]; then
+        git diff --exit-code >&2 || {
+            echo "Did you forget to build the manifests locally?" >&2;
+            echo "Please run ./hack/build-manifests.sh and update your PR" >&2;
+            exit 1;
+        }
+    fi
+    echo "changes are up to date" >&2
+}
+
+if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
+    main "$@"
+fi
