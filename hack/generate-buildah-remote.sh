e1e1099c9b547a12e66b577a20be13aa29c5a611
Adam Cmiel <acmiel@redhat.com>
2025-02-27 13:20:47 +0100

    generate-buildah-remote: set GOTOOLCHAIN=auto
    The generate-ta-tasks.sh script sets GOTOOLCHAIN=auto to avoid failing
    when the user has an older version of Go than the one declared in go.mod
    
    Do the same in the generate-buildah-remote script.
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- hack/generate-buildah-remote.sh
+++ hack/generate-buildah-remote.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd "${SCRIPTDIR}/../task-generator/remote"
-go build -o /tmp/remote-generator main.go
+GOTOOLCHAIN=auto GOSUMDB=sum.golang.org go build -o /tmp/remote-generator main.go
 
 for version in 0.1 0.2 0.3 0.4; do
     /tmp/remote-generator --buildah-task="${SCRIPTDIR}/../task/buildah/${version}/buildah.yaml" \
