67099b53d060db0e3436d6ff6fdd44cf0e092c7e
Filip Mudry <fmudry@redhat.com>
2025-05-05 12:36:28 +0200

    delete generate buildah remote versions
    I've left the version in the loop, in order to make it easier when we add a new version
    
    Signed-off-by: Filip Mudry <fmudry@redhat.com>

--- hack/generate-buildah-remote.sh
+++ hack/generate-buildah-remote.sh
@@ -6,7 +6,7 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "${SCRIPTDIR}/../task-generator/remote"
 GOTOOLCHAIN=auto GOSUMDB=sum.golang.org go build -o /tmp/remote-generator main.go
 
-for version in 0.1 0.2 0.3 0.4; do
+for version in 0.4; do
     /tmp/remote-generator --buildah-task="${SCRIPTDIR}/../task/buildah/${version}/buildah.yaml" \
         --remote-task="${SCRIPTDIR}/../task/buildah-remote/${version}/buildah-remote.yaml" --task-version="$version"
     /tmp/remote-generator --buildah-task="${SCRIPTDIR}/../task/buildah-oci-ta/${version}/buildah-oci-ta.yaml" \
