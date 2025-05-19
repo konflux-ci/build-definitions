f2d7b470600c8d8c004032447567ebfd9c7c4de3
Zhiming Xiong <zxiong@redhat.com>
2025-05-19 10:40:39 +0800

    feat(CLOUDDST-27284): improve scanning performance by using clamdscan
    This change replaces clamscan with clamdscan to speed up scans on large
    and multi-architecture container images.
    
    clamscan is single-threaded and cannot leverage multiple CPU cores,
    making it inefficient for scanning images with many files. It also
    requires reloading the virus database for each scan, increasing overhead.
    
    With clamdscan, scans are offloaded to the clamd daemon, which supports
    multi-threaded scanning and shared database access, significantly
    reducing scan time and CPU load.
    
    Additionally, this update introduces Tekton's matrix feature to run
    clamav-scan tasks in parallel—one per image architecture—greatly
    improving performance for multi-arch images.
    
    Also introduces a task migration script for the new version.
    
    Signed-off-by: Zhiming Xiong <zxiong@redhat.com>

---
+++ task/clamav-scan/0.3/migrations/0.3.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Created for task: clamav-scan@0.3
+# Creation time: 2025-07-21T01:11:59+00:00
+
+declare -r pipeline_file=${1:?missing pipeline file}
+TASK_NAME="clamav-scan"
+
+# Check if the pipeline has 'build-platforms' parameter
+if ! yq -e '.spec.params[] | select(.name == "build-platforms")' "$pipeline_file" >/dev/null 2>&1; then
+  echo "Matrix will not be added because the dependent parameter 'build-platforms' is not defined in the pipeline."
+  exit 0
+fi
+
+# Check if the task exists
+if ! yq -e '.spec.tasks[] | select(.name == "'"$TASK_NAME"'")' "$pipeline_file" >/dev/null 2>&1; then
+  echo "Task '$TASK_NAME' does not exist in the pipeline."
+  exit 0
+fi
+
+# Check if the task already has a matrix
+if yq -e '.spec.tasks[] | select(.name == "'"$TASK_NAME"'") | has("matrix")' "$pipeline_file" >/dev/null 2>&1; then
+  echo "Matrix already exists for task '$TASK_NAME'. No changes made."
+else
+  echo "Adding matrix for task '$TASK_NAME'..."
+  yq -i "
+  (.spec.tasks[] 
+    | select(.name == \"clamav-scan\" and .matrix == null)
+  ).matrix = {
+    \"params\": [
+      {
+        \"name\": \"image-arch\",
+        \"value\": [\"\$(params.build-platforms)\"]
+      }
+    ]
+  }
+" "$pipeline_file"
+
+  echo "Adding matrix for task '$TASK_NAME' completed!"
+fi
