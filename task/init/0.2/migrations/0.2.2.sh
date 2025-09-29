45a703273cb925006ef1a2e7b5c9f04b17d91394
Martin Basti <mbasti@redhat.com>
2025-09-29 14:37:11 +0200

    fix(init): hotfix for migration 0.2.1
    Older FBC pipelines without run-opm-command has been accidentally
    migrated to docker format. Fix this by changing default to oci.
    
    Signed-off-by: Martin Basti <mbasti@redhat.com>

---
+++ task/init/0.2/migrations/0.2.2.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Created for task: init@0.2.2
+# Creation time: 2025-09-29T15:00:03Z
+
+declare -r pipeline_file=${1:?missing pipeline file}
+
+
+# Fixing migration from 0.2.1, where old FBC pipelines has been accidentally migrated to docker
+
+if yq -e '.spec.params[] | select(.name == "buildah-format")' "$pipeline_file" >/dev/null; then
+    # migration happened
+    if yq -e '.spec.tasks[] | select(.taskRef.params[] | (.name == "name" and .value == "validate-fbc"))' "$pipeline_file" >/dev/null; then
+       # it's older FBC pipeline with migration, switch to oci
+       yq -i '(select( .spec.params[] | .name == "buildah-format")).default = "oci"' "$pipeline_file"
+       echo "Switching FBC pipeline back to OCI"
+    fi
+fi
+
