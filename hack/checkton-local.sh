7e06c30933fe90abce89a45c05d8c57e6edbed74
Tomáš Nevrlka <tnevrlka@redhat.com>
2025-12-08 14:30:40 +0100

    shared-ci: Add notice to almost unchanged files
    Some files in build-definitions are almost the same as in
    task-repo-shared-ci.
    
    Commit them with a notice that the file is now templated and very minor
    fixes.
    Let them be handled by cruft from now on.
    
    Signed-off-by: Tomáš Nevrlka <tnevrlka@redhat.com>

--- hack/checkton-local.sh
+++ hack/checkton-local.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -o errexit -o nounset -o pipefail
 
+# <TEMPLATED FILE!>
+# This file comes from the templates at https://github.com/konflux-ci/task-repo-shared-ci.
+# Please consider sending a PR upstream instead of editing the file directly.
+# See the SHARED-CI.md document in this repo for more details.
+
 get_checkton_image_based_on_action_version() {
     sed -nE \
         's;^\s*uses: (.*)/checkton.*(v[0-9]\S*);ghcr.io/\1/checkton:\2;p' \
