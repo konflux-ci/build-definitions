089af3361d0d3238cea2365a42bc602b4b67aa98
Tomáš Nevrlka <tnevrlka@redhat.com>
2025-12-08 14:40:49 +0100

    shared-ci: Add files with minor changes
    Some files in build-definitions are almost the same as in
    task-repo-shared-ci.
    
    Commit them with a notice that the file is now templated and very minor
    fixes.
    Let them be handled by cruft from now on.
    
    Signed-off-by: Tomáš Nevrlka <tnevrlka@redhat.com>

--- hack/verify-manifests.sh
+++ hack/verify-manifests.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
 
+# <TEMPLATED FILE!>
+# This file comes from the templates at https://github.com/konflux-ci/task-repo-shared-ci.
+# Please consider sending a PR upstream instead of editing the file directly.
+# See the SHARED-CI.md document in this repo for more details.
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 main() {
@@ -9,7 +14,6 @@ main() {
         git diff --exit-code >&2 || {
             echo "Did you forget to build the manifests locally?" >&2;
             echo "Please run ./hack/build-manifests.sh and update your PR" >&2;
-            echo "Or run ./hack/generate-everything.sh to run all the generators at once." >&2;
             exit 1;
         }
     fi
