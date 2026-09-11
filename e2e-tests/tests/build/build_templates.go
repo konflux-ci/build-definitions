f5e47231d3c4b90725bb0a208006ff30fa3f2865
jstuart <jstuart@redhat.com>
2026-09-11 08:17:01 -0500

    bump conforma version

--- e2e-tests/tests/build/build_templates.go
+++ e2e-tests/tests/build/build_templates.go
@@ -33,7 +33,7 @@ var (
 )
 
 const pipelineCompletionRetries = 2
-const verifyECTaskBundle = "quay.io/conforma/tekton-task:konflux@sha256:775cbfaa5361bd39d7cf8816c1c31b85c862609fb70ee76afa7d5a8b7d839517"
+const verifyECTaskBundle = "quay.io/conforma/tekton-task:konflux@sha256:e2ac3444a229ce1688c96692ba2af166656be9313ac5cba750cac1c7daf3371d"
 
 type TestBranches struct {
 	RepoName       string
@@ -405,7 +405,7 @@ var _ = framework.BuildSuiteDescribe("Build templates E2E test", ginkgo.Label("b
 						ecpSource := ecp.Source{
 							Config: &ecp.SourceConfig{
 								Include: []string{"@redhat"},
-								Exclude: []string{"cve", "hermetic_task", "labels", "trusted_task", "test", "base_image_registries.allowed_registries_provided", "base_image_registries.base_image_permitted:docker.io/library/ibmjava", "base_image_registries.base_image_permitted:docker.io/library/node",
+								Exclude: []string{"cve", "buildah_build_task.disallowed_platform_patterns_pattern", "git_branch", "rpm_build_deps", "hermetic_task", "labels", "trusted_task", "test", "base_image_registries.allowed_registries_provided", "base_image_registries.base_image_permitted:docker.io/library/ibmjava", "base_image_registries.base_image_permitted:docker.io/library/node",
 									"tasks.pinned_task_refs", "tasks.required_tasks_found", "tasks.required_untrusted_task_found", "slsa_build_scripted_build.image_built_by_trusted_task", "source_image.exists", "sbom_spdx.hermeto_attribution_required",
 									"sbom_spdx.allowed_package_sources:pkg:pypi/dockerfile-parse?checksum=sha256:36e4469abb0d96b0e3cd656284d5016e8a674cd57b8ebe5af64786fe63b8184d&download_url=https://github.com/containerbuildsystem/dockerfile-parse/archive/refs/tags/2.0.0.tar.gz",
 									"sbom_spdx.allowed_package_sources:pkg:generic/dependency-check.zip?checksum=sha256:c5b5b9e592682b700e17c28f489fe50644ef54370edeb2c53d18b70824de1e22&download_url=https://github.com/jeremylong/DependencyCheck/releases/download/v11.1.0/dependency-check-11.1.0-release.zip"},
